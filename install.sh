e4e13dc1466f60ebd82269cc7298b1951e29b877
lubien <lubien1996@gmail.com>
2016-06-27 22:51:50 -0300

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+# neovim
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
+# vim-plug
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# git 
+sudo apt-get install git
+
+# oceanic-next-shell
