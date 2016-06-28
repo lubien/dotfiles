280ccf5a23e1a3998c46dd73d2b9b4426976f4e2
lubien <lubien1996@gmail.com>
2016-06-27 22:57:07 -0300

    Remove trailing space

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get install neovim
 # vim-plug
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# git 
+# git
 sudo apt-get install git
 
 # oceanic-next-shell
