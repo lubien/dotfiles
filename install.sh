0c4ac53ce082ce8a076285b4f9c9e1fb1d910b8b
lubien <lubien1996@gmail.com>
2016-07-05 18:19:18 -0300

    Add powerline fonts installer

--- install.sh
+++ install.sh
@@ -45,6 +45,12 @@ else
 	sudo apt-get install zsh
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	chsh -s $(which zsh)
+
+	# Powerline fonts
+	wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+	wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+	mv PowerlineSymbols.otf ~/.fonts/
+	mkdir -p .config/fontconfig/conf.d
 fi
 
 stow bin
