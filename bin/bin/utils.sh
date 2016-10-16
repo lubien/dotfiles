bf8a46292d9c8dd6acb4d80ec99a7eee036f36ad
lubien <lubien1996@gmail.com>
2016-10-16 10:12:37 -0300

    Support force in nvimOrLs

--- bin/bin/utils.sh
+++ bin/bin/utils.sh
@@ -1,14 +1,14 @@
 function nvimOrLs {
-    folder=$1
-    sufix=$2
-    possible_file=$folder/$3$sufix
+	folder=$1
+	sufix=$2
+	possible_file=$folder/$3$sufix
+	force=$4
 
-    if [ -e $possible_file ]
-    then
-	nvim $possible_file
-    else
-	echo "File not found. Listing possible files:"
-	ls $folder
-    fi
+	if [ -e $possible_file ] || [ $force == '-f' ]
+	then
+		nvim $possible_file
+	else
+		echo "File not found. Listing possible files:"
+		ls $folder
+	fi
 }
-
