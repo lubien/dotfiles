8ed1bea0b1534ad359e5be933b5aca9504810b4b
lubien <lubien1996@gmail.com>
2017-10-05 22:07:02 -0300

    Move git and fzf configs to /home/lubien

--- .fzf.sh
+++ .fzf.sh
@@ -6,19 +6,19 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/joao/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/joao/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" == */home/joao/.fzf/man* && -d "/home/joao/.fzf/man" ]]; then
-  export MANPATH="$MANPATH:/home/joao/.fzf/man"
+if [[ ! "$MANPATH" == *$HOME/.fzf/man* && -d "$HOME/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:$HOME/.fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/joao/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
