6081ea4e9cfd44c9c7e06b98a88721029b65110e
João Ferreira <lubien1996@gmail.com>
2017-11-30 16:50:12 -0300

    Create fix-spider-ls00046-monitor.sh

---
+++ bin/fix-spider-ls00046-monitor.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+# Life is too short to type this every time I use this computer
+
+# Full HD
+# xrandr --newmode "1920x1080_60.00"  172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync
+# xrandr --addmode VGA1 1920x1080_60.00
+# xrandr --output VGA1 --mode 1920x1080_60.00
+
+# HD
+xrandr --newmode "1440x900_60.00"  106.47  1440 1520 1672 1904  900 901 904 932  -HSync +Vsync
+xrandr --addmode VGA1 1440x900_60.00
+xrandr --output VGA1 --mode 1440x900_60.00
