353b3d4fa076b836247db2d7673779d5cae17018
lubien <lubien1996@gmail.com>
2017-12-27 09:29:25 -0300

    Send rg errors to /dev/null
    It was making impossible to enter on a valid file because of the errored ones

--- .fzf.sh
+++ .fzf.sh
@@ -3,7 +3,7 @@
 # `rg` does respect .gitignore but only if you're running
 # `rg` in the current folder with a .gitignore file.
 # export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --glob "!.git/*" --glob "!node_modules/*" --glob "!_build"'
-export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Auto-completion
